076e6df24fe694b1418fb465e90252ef3137d651
Sujeet Kumar <sujeetk69@gmail.com>
2022-09-02 15:35:00 +0100

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+console.log("JS Loaded");
+var start = new Date("Aug 31 2022 17:22:00 GMT+0100").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var t = now - start;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
