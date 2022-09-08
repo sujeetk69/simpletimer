ce62bd0dff66dcfafdcbc21aa1f05634042c38c1
Sujeet Kumar <sujeetk69@gmail.com>
2022-09-08 17:40:57 +0100

    Add joke feature JS

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,37 @@ var x = setInterval(function () {
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 }, 1000);
+
+function updateJokeOnUi(jokeText) {
+    let jokeElem = document.getElementById("joke");
+    jokeElem.innerHTML = jokeText;
+}
+
+function getRandomJoke() {
+    const url = "https://v2.jokeapi.dev/joke/Any";
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            let jokeObj = JSON.parse(xmlHttp.responseText);
+            // console.log(jokeObj);
+            let jokeText = "";
+            if (jokeObj.type == "single") {
+                jokeText = jokeObj.joke;
+            } else {
+                jokeText = "Q. " + jokeObj.setup + "<br> A. " + jokeObj.delivery;
+            }
+            updateJokeOnUi(jokeText);
+        }
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
+
+// Add event listener for the new joke button
+var button = document.getElementById("newJokeBtn");
+button.addEventListener("click", function (event) {
+    getRandomJoke();
+});
+
+getRandomJoke();
